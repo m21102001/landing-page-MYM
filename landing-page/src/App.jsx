ed3b50cab3c256810eeae25765430fe55fd4b54c
m21102001 <mohammedtarek21102001@gmail.com>
2023-07-18 23:45:32 +0300

    testing fail
    testing fail

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,32 +1,12 @@
+import Navbar from './navbar/Navbar'
+
+
 import './App.css'
 
 function App() {
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-        <div>
-          <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
-            <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"></svg>
-          </span>
-        </div>
-        <h3 className="text-slate-900 dark:text-white mt-5 text-base font-medium tracking-tight">Writes Upside-Down</h3>
-        <p className="text-slate-500 dark:text-slate-400 mt-2 text-sm">
-          The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
-        </p>
-      </div>
-      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-  <div className="shrink-0">
-    <img className="h-12 w-12" src="./assets/Huawei.png" alt="ChitChat Logo"/>
-  </div>
-  <div>
-    <div className="text-xl font-medium text-black">ChitChat</div>
-    <p className="text-slate-500">You have a new message!</p>
-  </div>
-</div>
+     <Navbar/>
     </>
   )
 }
