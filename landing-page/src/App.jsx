e9d055bf19971bdef2abf4ec22bedb7626652ed9
aymanhossam55 <100770611+aymanhossam55@users.noreply.github.com>
2023-07-20 03:16:15 +0300

    Update App.jsx

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './Components/header/Header'
+import LandTeam from './Components/LandTeam'
 
 function App() {
 
   return (
       <div className='bg-white h-screen w-screen dark:bg-slate-800'>
         <Header/>
+        <LandTeam/>
       </div>
   )
 }
