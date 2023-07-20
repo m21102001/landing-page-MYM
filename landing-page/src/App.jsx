45d27ce8dca98f89b040fbf9848d98c27af24a71
adelahmed0 <adel15952@gmail.com>
2023-07-21 01:51:15 +0300

    add TechnologiesUsed section

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css'
 import Header from './Components/header/Header'
-import TechnologiesUsed from "./Components/TechnologiesUsed/TechnologiesUsed.jsx";
-import LandTeam from "./Components/LandTeam.jsx";
 
 function App() {
 
   return (
       <div className='bg-white h-full dark:bg-slate-800'>
         <Header/>
-          <LandTeam/>
-          <TechnologiesUsed/>
       </div>
   )
 }
