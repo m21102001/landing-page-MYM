0b272328029ae8fc13b17f7d68d038996a43e7b9
adelahmed0 <adel15952@gmail.com>
2023-07-20 20:56:33 +0300

    add class h-full

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './Components/header/Header'
+import LandTeam from "./Components/LandTeam.jsx";
+import TechnologiesUsed from "./Components/TechnologiesUsed/TechnologiesUsed.jsx";
 
 function App() {
 
   return (
-      <div className='bg-white h-screen w-screen dark:bg-slate-800'>
+      <div className='bg-white h-full dark:bg-slate-800'>
         <Header/>
       </div>
   )
