5ab7b42e1395cdc71c8a81c8bb94c1b4d3464ca4
adelahmed0 <adel15952@gmail.com>
2023-07-20 20:56:58 +0300

    add header

--- landing-page/src/Components/header/Header.jsx
+++ landing-page/src/Components/header/Header.jsx
@@ -1,21 +1,23 @@
 import Navbar from "../navbar/Navbar"
+import './headar.css'
 
 const Header = () => {
     return (
-        <container className='header h-screen w-screen bg-light-800 '>
+        <div className='header h-full'>
             <Navbar />
-            <div>
-                <h1 className="text-center">ابحث عن أفضل الحلول البرمجية</h1>
-                <h5>خيارك الاول لتحول فكرتك الي واقع مع افضل سعر ل اعلي جودة</h5>
+            <div className='flex container mx-auto justify-between'>
+                <div className='header-image'>
+                    <img className='' src='src/assets/images/Saly-19.png' alt='image'/>
+                </div>
+                <div className='header-text text-white'>
+                    <h1 className="text-5xl font-bold text-white">
+                        ابحث عن أفضل <span className='text-blue-600 border-green-800 p-1 border-2'>الحلول</span> البرمجية
+                    </h1>
+                    <h5 className='my-4'>خيارك الاول لتحول فكرتك الي واقع مع افضل سعر ل اعلي جودة</h5>
+                    <button>الحصول علي استشارة</button>
+                </div>
             </div>
-            <input type="button" value="الحصول علي استشارة" disabled className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
-      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-      invalid:border-pink-500 invalid:text-pink-600
-      focus:invalid:border-pink-500 focus:invalid:ring-pink-500
-    "/>
-
-        </container>
+        </div>
     )
 }
 
