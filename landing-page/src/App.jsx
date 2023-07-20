f90e23335d03ba7401c9b981b734545b0b5da9d4
adelahmed0 <adel15952@gmail.com>
2023-07-20 21:11:37 +0300

    fixed

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Header from './Components/header/Header'
+import TechnologiesUsed from "./Components/TechnologiesUsed/TechnologiesUsed.jsx";
+import LandTeam from "./Components/LandTeam.jsx";
 
 function App() {
 
   return (
-      <div className='bg-white h-screen w-screen dark:bg-slate-800'>
+      <div className='bg-white h-full dark:bg-slate-800'>
         <Header/>
+          <LandTeam/>
+          <TechnologiesUsed/>
       </div>
   )
 }
