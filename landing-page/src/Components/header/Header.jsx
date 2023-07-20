7eeb44c233be866e2308a9a99cd78bfd738a553e
adelahmed0 <adel15952@gmail.com>
2023-07-21 01:52:15 +0300

    fixed header
    fixed header

--- landing-page/src/Components/header/Header.jsx
+++ landing-page/src/Components/header/Header.jsx
@@ -6,15 +6,15 @@ const Header = () => {
         <div className='header h-full'>
             <Navbar />
             <div className='flex container mx-auto justify-between'>
-                <div className='header-image'>
-                    <img className='' src='src/assets/images/Saly-19.png' alt='image'/>
-                </div>
                 <div className='header-text text-white'>
                     <h1 className="text-5xl font-bold text-white">
                         ابحث عن أفضل <span className='text-blue-600 border-green-800 p-1 border-2'>الحلول</span> البرمجية
                     </h1>
                     <h5 className='my-4'>خيارك الاول لتحول فكرتك الي واقع مع افضل سعر ل اعلي جودة</h5>
                     <button>الحصول علي استشارة</button>
+                </div>
+                <div className='header-image'>
+                    <img className='' src='src/assets/images/Saly-19.png' alt='image'/>
                 </div>
             </div>
         </div>
