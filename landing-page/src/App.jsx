de21187d0ed02fbd5c098fb6506efebddf574b17
aymanhossam55 <100770611+aymanhossam55@users.noreply.github.com>
2023-07-21 16:14:47 +0300

    Update App.jsx

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
+import Aboutus from './Components/Aboutus'
 import Developerflutter from './Components/Developerflutter'
 import Header from './Components/header/Header'
+import Helpus from './Components/Helpus'
 import LandTeam from './Components/LandTeam'
+import TechnologiesUsed from './Components/TechnologiesUsed/TechnologiesUsed'
 
 function App() {
 
   return (
       <div className='bg-white h-full dark:bg-slate-800'>
         <Header/>
+        <Aboutus/>
+        <Helpus/>
+        <LandTeam/>
+        <TechnologiesUsed/>
         <Developerflutter/>
       </div>
   )
