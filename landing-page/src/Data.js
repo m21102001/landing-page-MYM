e12310acae22caedf76e51da381e4c1f64a9db1d
adelahmed0 <adel15952@gmail.com>
2023-07-22 10:25:52 +0300

    edit helpus
    edit helpus

--- landing-page/src/Data.js
+++ landing-page/src/Data.js
@@ -1,44 +1,5 @@
 import { number } from "prop-types"
 
-export const helpus = [
-    {
-      heading: 'الاتصال بك',
-      description: 'أخبرنا بأي شئ تريد تصميمه',
-      image: "assets/call.png",
-      number: 1
-    },
-    {
-      heading: 'الخطة',
-      description: 'سوف نقوم بعمل خطه بناء متكامله',
-      image: "assets/plan.png",
-      number: 2
-    },
-    {
-      heading: 'التصميم',
-      description: 'تصميم الواجهات تصميم مرن و فعال مع المستخدم',
-      image: "assets/design.png",
-      number: 3
-    },
-    {
-      heading: 'البرمجة',
-      description: 'نقوم بكتابه الكود و عمل قواعد البيانات و ربطها',
-      image: "assets/programming.png",
-      number : 4
-    },
-    {
-      heading: 'الفحص النهائى',
-      description: 'سوف نقوم بفحص الكود و تأمينه من الثغرات',
-      image: "assets/finalCheck.png",
-      number : 5
-    },
-    {
-      heading: 'النشر',
-      description: 'سوف نقوم برفع التطبيق او الموقع و نشرة لتبدا بجني الارباح',
-      image: "assets/publish.png",
-      number: 6
-    },
-
-  ]
   export const aboutus = [
     {
       heading: '10+',
